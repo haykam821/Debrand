18ffb3b4c106c8b56a8fe02b9e7f8feb6d871b08
joshcangit <yjoshua59@gmail.com>
2021-09-16 21:04:39 +0800

    Upgrade to 1.17
    Use newer maven URLs
    Use com.terraformersmc.modmenu
    Add icon from CurseForge

--- src/main/java/io/github/haykam821/debrand/config/ModMenuCompatibility.java
+++ src/main/java/io/github/haykam821/debrand/config/ModMenuCompatibility.java
@@ -1,20 +1,21 @@
 package io.github.haykam821.debrand.config;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
+@SuppressWarnings("deprecation")
 @Environment(EnvType.CLIENT)
 public class ModMenuCompatibility implements ModMenuApi {
-	@Override
-	public String getModId() {
-		return "debrand";
-	}
 
-	@Override
-	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return screen -> AutoConfig.getConfigScreen(ModConfig.class, screen).get();
-	}
+    public String getModId() {
+        return "debrand";
+    }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return screen -> AutoConfig.getConfigScreen(ModConfig.class, screen).get();
+    }
 }
